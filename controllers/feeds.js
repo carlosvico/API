384e7075de582798f11ac1c03e88bca81ecda96e
carlosvico <calladrianjr@gmail.com>
2022-01-11 11:36:27 -0300

    Implementado padrão MVC

--- controllers/feeds.js
+++ controllers/feeds.js
@@ -1,6 +1,7 @@
 
+
 exports.pegandoPosts = (req, res, next) => {
-    // PEGANDO UM POST
+    // Simulando uma resposta formato json vinda do db para o cliente
 
     res.status(200).json({
         posts: [{title: 'Primeiro Post', content: 'Este é o primeiro post!'},
@@ -13,7 +14,8 @@ exports.pegandoPosts = (req, res, next) => {
 exports.criandoPost = (req, res, next) => {
     const titulo = req.body.title;
     const conteudo = req.body.content;
-    // Criando posts in db
+
+    // Simula a inserção de um registro do cliente para o db em formato json
     res.status(201).json({
         message: 'Post criado com sucesso!', 
         post: {id: new Date().toISOString(), title:titulo, content: conteudo}
